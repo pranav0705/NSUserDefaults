6e7294030f48489dced2a5e69c98154b80f3cd4b
Pranav Bhandari <pranavbhandari@Pranavs-MacBook-Pro.local>
2017-04-02 23:04:37 -0400

    completed

--- NSUserDefaults/ViewController.swift
+++ NSUserDefaults/ViewController.swift
@@ -10,14 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theme: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var themeSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let value = UserDefaults.standard.value(forKey: "ThemeOn") {
+            if(value as! Bool){
+            
+                themeSwitch.setOn(true, animated: false)
+            }
+            else{
+                UserDefaults.standard.set(true, forKey: "ThemeOn")
+                themeSwitch.setOn(false, animated: false)
+            }
+        }
+        switchFlipped(sender: self)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func switchFlipped(sender: AnyObject){
+        if themeSwitch.isOn{
+            theme.text = "DayLight Theme is On"
+            img.image = UIImage(named: "Sun")
+            view.backgroundColor = UIColor(red: 129/255, green: 243/255, blue: 253/255, alpha: 1)
+            UserDefaults.standard.set(true, forKey: "ThemeOn")
+        }
+        else{
+            theme.text = "MidNight Theme is On"
+            img.image = UIImage(named: "moon")
+            view.backgroundColor = UIColor(red: 88/255, green: 86/255, blue: 214/255, alpha: 1)
+            UserDefaults.standard.set(false, forKey: "ThemeOn")
+        }
     }
 
 
